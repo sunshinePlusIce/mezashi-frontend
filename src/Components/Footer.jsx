6cb0aade8a1bfe14c2eab4adaf0cb57b041d3efc
sunshinePlusIce <sunshineplusice@gmail.com>
2023-04-11 00:27:47 +0800

    seperate Footer into single component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Layout } from "antd";
+const { Footer } = Layout;
+
+export default function MyFooter() {
+    return (
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+
+          Mezashi ©2023 Created by SunshinePlusIce
+        </Footer>
+    )
+}
+
+
