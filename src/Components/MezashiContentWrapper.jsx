422fe7d63ee21ad0c1ecbefeb1e9ea78e4c128e6
sunshinePlusIce <sunshineplusice@gmail.com>
2023-04-12 14:45:27 +0800

    add a wrapper to display mezashi information

---
+++ src/Components/MezashiContentWrapper.jsx
@@ -0,0 +1,40 @@
+import { Tag, Space } from "antd"
+import { CalendarOutlined } from "@ant-design/icons"
+import { CompleteCondition } from "../Constant/Constant"
+
+export default function MezashiContentWrapper({mezashi}) {
+   return (
+    <div className="mezashi-treenode">
+        <div className="mezashi-name">
+            <span className="fw-bold">{mezashi.name}</span>
+            {
+                mezashi.completeCondition === CompleteCondition.SPECIFIC_DATE ? (
+                    <Tag icon={<CalendarOutlined />} color="default" className="ms-2">
+                        {mezashi.targetDate}
+                    </Tag>
+                )
+                : (
+                    <Tag color="default" className="ms-2">
+                        {
+                            mezashi.completeCondition.split("_").join(" ").toLowerCase()
+                        }
+                    </Tag>
+                )
+            }
+            
+        </div>
+        { 
+            mezashi.tags && mezashi.tags.length > 0 && 
+            <Space size={[0, mezashi.tags.length]} className="mezashi-tags" wrap>
+                {
+                    mezashi.tags.map(tag => {
+                        return <Tag color={tag.tagColor.toLowerCase()}>{tag.name}</Tag>
+                    })
+                }
+            </Space>
+        } 
+    </div>
+    
+    
+   ) 
+}
