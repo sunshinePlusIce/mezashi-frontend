be22a645c765fd9daa355e9b96b2c48a6f084425
sunshinePlusIce <sunshineplusice@gmail.com>
2023-04-12 21:04:12 +0800

    add new constants

--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -5,3 +5,47 @@ export const CompleteCondition = {
     CASCADE: "CASCADE",
     PICK: "PICK"
 }
+
+export const CompleteConditionArray = [
+    CompleteCondition.MANUAL,
+    CompleteCondition.SPECIFIC_DATE,
+    CompleteCondition.CASCADE,
+    CompleteCondition.PICK
+]
+
+export const DefaultTags = [
+    {
+        name: "work",
+        tagColor: "BLUE"
+    }, {
+        name: "life",
+        tagColor: "CYAN"
+    }
+]
+
+export const supportedColors = {
+    RED: "RED",
+    VOLCANO: "VOLCANO",
+    ORANGE: "ORANGE",
+    GOLD: "GOLD",
+    LIME: "LIME",
+    GREEN: "GREEN",
+    CYAN: "CYAN",
+    BLUE: "BLUE",
+    GEEKBLUE: "GEEKBLUE",
+    PURPLE: "PURPLE"
+}
+
+export const supportedColorsArray = [
+    supportedColors.RED,
+    supportedColors.VOLCANO,
+    supportedColors.ORANGE,
+    supportedColors.GOLD,
+    supportedColors.LIME,
+    supportedColors.GREEN,
+    supportedColors.CYAN,
+    supportedColors.BLUE,
+    supportedColors.GEEKBLUE,
+    supportedColors.PURPLE
+]
+
